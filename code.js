3d6050ee50b57cda9858bc6da65a1284d790e3ea
Shankk <brianviveiros@hotmail.com>
2023-05-07 23:03:03 -0400

    added header, buttons for login and new book

---
+++ code.js
@@ -0,0 +1,13 @@
+let Library = [];
+
+function Book(author,title,numPages, hasRead) {
+    this.author = author
+    this.title = title
+    this.numPages = numPages
+    this.hasRead = hasRead
+}
+
+function addBookToLibrary() {
+    
+}
+    
